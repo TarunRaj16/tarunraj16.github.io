a594e73ba4f4d5c9be11468bd472406fb2d08f1f
TarunRaj16 <tarunrajendran@gmail.com>
2014-09-20 23:35:13 -0500

    Seperated the style and CSS from index.html

---
+++ indexjs.js
@@ -0,0 +1,15 @@
+	$(document).ready(function() {
+ 
+				$("#owl-example").owlCarousel({
+			
+				autoPlay:true,
+				navigation:false,
+				slideSpeed:300,
+				paginationSpeed:1000,
+				singleItem:true,
+				itemsScaleUp:false,
+				stopOnHover:true
+			
+				});
+ 
+			});
